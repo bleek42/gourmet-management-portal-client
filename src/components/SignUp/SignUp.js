03dc6b699498ca0c411dbf9a0f308c9df295b567
bleek42 <brandonleek42@gmail.com>
2020-09-29 14:49:19 -0400

    signup.js > added ev handler post req new user

--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -1,7 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Grid, FormControl, InputLabel, Input, FormHelperText } from '@material-ui/core';
+import { API_TOKEN, API_URL } from '../../config';
 
 const SignUp = () => {
+
+  const [signUp, setSignUp] = useState([]);
+
+  const submitSignup = async (ev) => {
+    ev.preventDefault();
+    const { username, email, password, pwCheck } = ev.target.value;
+    if (password === pwCheck) {
+      alert('passwords do not match!')
+    }
+    try {
+      const req = {
+        headers: {
+          Authorization: `Bearer ${API_TOKEN}`,
+        },
+        body: {
+          username: JSON.stringify(username),
+          email: JSON.stringify(email),
+          password: JSON.stringify(password)
+        }
+      }
+      const res = await fetch(req, `${API_URL}/users/new`);
+      const data = await res.json();
+      setSignUp([data]);
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
   return (
     <Grid
       container
@@ -10,10 +39,11 @@ const SignUp = () => {
       alignItems="center"
     >
       <Grid item>
-        <FormControl>
-          <InputLabel></InputLabel>
-          <Input></Input>
-          <FormHelperText></FormHelperText>
+        <FormControl onSubmit={submitSignup}>
+          <Input name="username" placeholder="Username" type="text"></Input>
+          <Input name="email" placeholder="Email" type="text" />
+          <Input name="password" placeholder="Password" type="text" />
+          <Input name="pwCheck" placeholder="Re-Enter Password" type="text" />
         </FormControl>
       </Grid>
     </Grid>
